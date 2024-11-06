dc845eb6182776b29b8c581c27398d753c1ed910
Ekaterina Polyntseva <ekaterina.polynceva@gmail.com>
2024-11-06 11:10:31 +0300

    fix deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,12 @@
 import "./App.css";
-import { useDispatch, useSelector } from "react-redux";
-
-import { changelanguage } from "./store/reducers/languageSlice";
-import i18n from "./components/i18n/index";
-import { useTranslation } from "react-i18next";
-
-import { Projects } from "./components/Projects/Projects";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home/Home";
 
 function App() {
-  const { t } = useTranslation();
-  const dispatch = useDispatch();
-  const lang = useSelector((state) => state.lang.language);
-
-  const chooseLanguage = (evt) => {
-    evt.preventDefault();
-    i18n.changeLanguage(evt.target.value); // i18n.changeLanguage() is used to change the language assigned to lng in i18n.js file.
-    dispatch(changelanguage(evt.target.value));
-  };
   return (
-    <>
-      <select defaultValue={lang} onChange={chooseLanguage}>
-        <option value="en">{t("LanguageEng")}</option>
-        <option value="ru">{t("LanguageRu")}</option>
-      </select>
-      <h1>{t("Title")}</h1>
-      <Projects/>
-    </>
+    <Routes>
+      <Route path="/portfolio/" element={<Home />} />
+    </Routes>
   );
 }
 
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,30 @@
+import { useDispatch, useSelector } from "react-redux";
+
+import { changelanguage } from "../../store/reducers/languageSlice";
+import i18n from "../../components/i18n/index";
+import { useTranslation } from "react-i18next";
+
+import { Projects } from "../../components/Projects/Projects";
+
+export const Home = () => {
+    const { t } = useTranslation();
+    const dispatch = useDispatch();
+    const lang = useSelector((state) => state.lang.language);
+  
+    const chooseLanguage = (evt) => {
+      evt.preventDefault();
+      i18n.changeLanguage(evt.target.value); // i18n.changeLanguage() is used to change the language assigned to lng in i18n.js file.
+      dispatch(changelanguage(evt.target.value));
+    };
+
+    return (
+        <>
+          <select defaultValue={lang} onChange={chooseLanguage}>
+            <option value="en">{t("LanguageEng")}</option>
+            <option value="ru">{t("LanguageRu")}</option>
+          </select>
+          <h1>{t("Title")}</h1>
+          <Projects/>
+        </>
+      );
+}
